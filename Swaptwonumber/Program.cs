b831cf883b0d211087aa65f14e1837e1af90c1e1
Prabal2207 <prabal760@gmail.com>
2021-07-08 08:02:24 +0530

    [Prabal] Added:-Two numbers  swapping is done

---
+++ Swaptwonumber/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Swaptwonumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SwapTwoNumber();
+        }
+
+        public static void SwapTwoNumber()
+        {
+            int temp;
+            Console.WriteLine("Enter number 1:");
+            int A = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter number 2:");
+            int B = Convert.ToInt32(Console.ReadLine());
+            temp = A;
+            A = B;
+            B = temp;
+
+           
+
+            
+
+            Console.WriteLine("number 1 is " + A);
+            Console.WriteLine("number 2 is" + B);
+        }
+    }
+}
