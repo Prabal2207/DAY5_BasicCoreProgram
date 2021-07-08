cbfddba1113e96f09093f748afb3548f31ef8836
Prabal2207 <prabal760@gmail.com>
2021-07-08 08:05:09 +0530

    [Prabal] Added:-Number is checked for odd and even

---
+++ OddEven/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace OddEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OddEven();
+        }
+        public static void OddEven()
+        {
+            Console.WriteLine("Enter number 1:");
+            int A = Convert.ToInt32(Console.ReadLine());
+            if(A%2 == 0)
+            {
+                Console.WriteLine("Number is even");
+            }
+            else
+            {
+                Console.WriteLine("Number is odd");
+            }
+        }
+    }
+}
