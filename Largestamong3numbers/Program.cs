c6dadbf001ad6c1869baad2bfef02c80c6fbe6ae
Prabal2207 <prabal760@gmail.com>
2021-07-08 08:07:00 +0530

    [Prabal] Added:-Largest among three number is calculated

---
+++ Largestamong3numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Largestamong3numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            largest();
+        }
+        public static void largest()
+        {
+            Console.WriteLine("Enter number 1:");
+            int A = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter number 2:");
+            int B = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter number 3:");
+            int C = Convert.ToInt32(Console.ReadLine());
+            int largest = A > B ? (A > C ? A : C) : (B > C ? B : C);
+            Console.WriteLine("Largest number is" + largest);
+        }
+    }
+}
