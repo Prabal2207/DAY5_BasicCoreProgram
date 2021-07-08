0b4f44735565ef51cdb4c385f1d7bac4df21944a
Prabal2207 <prabal760@gmail.com>
2021-07-08 07:59:25 +0530

    [Prabal] Added:-Nth harmonic number is computed

---
+++ HarmonicNumber/HarmonicNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace HarmonicNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            nthHarmonic();
+        }
+
+       
+       public static void nthHarmonic()
+        {
+            Console.WriteLine("Enter N");
+            int n=Convert.ToInt32(Console.ReadLine());
+            float harmonic = 1;
+
+            for (int i = 2; i <= n; i++)
+            {
+                harmonic += (float)1 / i;
+            }
+
+            Console.WriteLine("Nth Harmonic Value is : " + harmonic);
+        }
+    }
+}
