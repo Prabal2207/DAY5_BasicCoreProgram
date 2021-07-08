440bea1ef8b5122a83a874e8cb098d480d3cf5c9
Prabal2207 <prabal760@gmail.com>
2021-07-08 08:01:27 +0530

    [Prabal] Added:-Quotient and Remainder of number is calculated

---
+++ QuotientRemainder/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace QuotientRemainder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Find();
+        }
+        public static void Find()
+        {
+            int quotient, remainder;
+            Console.WriteLine("Enter number 1:");
+            int A = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter number 2:");
+            int B = Convert.ToInt32(Console.ReadLine());
+            quotient = A / B;
+
+            remainder = A % B;
+
+            Console.WriteLine("Quotient is " + quotient);
+            Console.WriteLine("Remainder is" + remainder);
+        }
+    }
+}
