f18b6d093693100a8d3a9b240a7d65f5d5b1de69
Prabal2207 <prabal760@gmail.com>
2021-07-08 08:00:32 +0530

    [Prabal] Added:-Prime factors of number is computed

---
+++ Factors/Factors/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Factors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Factors();
+
+        }
+
+        public static void Factors()
+        {
+
+            Console.WriteLine("Enter number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= number; i++)
+            {
+                if (number % i == 0)
+                {
+                    int count = 0;
+                    for(int j=1;j<=i;j++)
+                    { 
+                        if(i%j == 0)
+                        {
+                            count++;
+                        }
+                        
+                    }
+                    if (count == 2)
+                    {
+                        Console.WriteLine(i + " is Prime Factor : " + number);
+                    }
+                }
+
+            }
+            
+        }
+    }
+}
