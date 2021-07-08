0e54cc94a708704b3a473e517dc49586c2812114
Prabal2207 <prabal760@gmail.com>
2021-07-08 08:06:09 +0530

    [Prabal] Added:-Alphabet is checked for vowel or consonent

---
+++ AlphabetVowelorConsonent/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AlphabetVowelorConsonent
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Alphabet");
+            char a = Convert.ToChar(Console.ReadLine());
+            vowelOrConsonant(a);
+        }
+        static void vowelOrConsonant(char x)
+        {
+            if (x == 'a' || x == 'e' || x == 'i' ||
+                              x == 'o' || x == 'u')
+                Console.WriteLine("Vowel");
+            else
+                Console.WriteLine("Consonant");
+        }
+    }
+}
