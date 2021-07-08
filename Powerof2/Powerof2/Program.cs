5069e1d6207586b2c09867e25d2911c4fb7da785
Prabal2207 <prabal760@gmail.com>
2021-07-08 07:58:42 +0530

    [Prabal] Added:-Power of 2 is calculated till limit < 31

---
+++ Powerof2/Powerof2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Powerof2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Powerof2();
+        }
+
+        public static void Powerof2()
+        {
+            int i = 0;
+
+            Console.WriteLine("Enter n");
+            double number = Convert.ToInt32(Console.ReadLine());
+            double pow = Math.Pow(2, number);
+
+            if (number >= 0 && number < 31)
+
+            { 
+                for (i = 1; i < number; i++)
+                {
+                    int result = 2 * i;
+                    Console.WriteLine("table of 2 at " + i);
+                    Console.WriteLine(result);
+                    if (result == pow)
+                    {
+                        break;
+                    }
+
+                }
+
+
+            }
+            else
+            {
+                Console.WriteLine("Limit exceded");
+            }
+        }
+    }
+}
